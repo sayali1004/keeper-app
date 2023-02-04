406eda756520429760bd019137102e217a43a146
Sayali Shelke <57850705+sayali1004@users.noreply.github.com>
2023-02-04 13:10:13 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./footer";
+import Note from "./Note";
+
+function App(){
+return (
+    <div className="note">
+    <Header />
+    <Note />
+    <Footer />
+    </div>
+  );
+
+
+
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+  return(
+<div>
+<h1>This is title</h1>
+<p>This is paragraph</p>
+</div>
+
+  );
+}
+
+export default Note;
